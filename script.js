7ce32e13a28df9df70d300250e24eb9e5786a73b
Fabian Gurth <fabigurth@googlemail.com>
2021-05-11 20:26:12 +0200

    bg pokemon

--- script.js
+++ script.js
@@ -1,11 +1,102 @@
 let currentPokemon;
+let pokemonTypes = [
+    {
+        "type": "bug",
+        "color": "#94BC4A"
+    },
+
+    {
+        "type": "dark",
+        "color": "#736C75"
+    },
+
+    {
+        "type": "dragon",
+        "color": "#6A7BAF"
+    },
+
+    {
+        "type": "electric",
+        "color": "#E5C531"
+    },
+
+    {
+        "type": "fairy",
+        "color": "#E397D1"
+    },
+
+    {
+        "type": "fighting",
+        "color": "#CB5F48"
+    },
+
+    {
+        "type": "fire",
+        "color": "#EA7A3C"
+    },
+
+    {
+        "type": "flying",
+        "color": "#7DA6DE"
+    },
+
+    {
+        "type": "ghost",
+        "color": "#846AB6"
+    },
+
+    {
+        "type": "grass",
+        "color": "#71C558"
+    },
+
+    {
+        "type": "ground",
+        "color": "#CC9F4F"
+    },
+
+    {
+        "type": "ice",
+        "color": "#70CBD4"
+    },
+
+    {
+        "type": "normal",
+        "color": "#AAB09F"
+    },
+
+    {
+        "type": "poison",
+        "color": "#B468B7"
+    },
+
+    {
+        "type": "psychic",
+        "color": "#E5709B"
+    },
+
+    {
+        "type": "rock",
+        "color": "#B2A061"
+    },
+
+    {
+        "type": "steel",
+        "color": "#89A1B0"
+    },
+
+    {
+        "type": "water",
+        "color": "#539AE2"
+    }
+];
 
 async function loadPokemon(){
-    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let url = 'https://pokeapi.co/api/v2/pokemon/10';
     let response = await fetch(url);
     currentPokemon = await response.json();
     console.log(currentPokemon);
-
+    setBgColor();
     renderPokemonInfo();
 }
 
@@ -18,13 +109,23 @@ function renderPokemonInfo(){
     renderAbout();
 }
 
+function setBgColor(){
+    let pokemonType = currentPokemon['types']['0']['type']['name']
+    pokemonTypes.forEach(info => {
+        if(pokemonType === info['type']){
+            document.getElementById('pokemon').style.backgroundColor = info['color'];
+        }
+    });
+}
+
 function renderName(){
     let name = currentPokemon['name'];
     document.getElementById('pokemonName').innerHTML = name.charAt(0).toUpperCase() + name.slice(1);
 }
 
 function renderImg(){
-    let img = currentPokemon['sprites']['front_default'];
+    
+    let img = currentPokemon['sprites']['other']['dream_world']['front_default'];
     document.getElementById('pokemonImg').src = img;
 }
 
@@ -47,8 +148,8 @@ function renderId(){
 async function renderAbout(){
     let species = await getSpeciesData(currentPokemon['id']);
     document.getElementById('species').innerHTML = species['genera']['7']['genus'];
-    document.getElementById('height').innerHTML = currentPokemon['height'];
-    document.getElementById('weight').innerHTML = currentPokemon['weight'];
+    document.getElementById('height').innerHTML = `${currentPokemon['height'] / 10} m`;
+    document.getElementById('weight').innerHTML = `${currentPokemon['weight'] / 10} kg`;
     renderAbilities();
 }
 
